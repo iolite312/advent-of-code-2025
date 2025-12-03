7c6cb0d6e09c844af972e862c6165a52c3c121e7
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-12-03 14:48:22 +0100

    day 1 part 2

---
+++ day-1/part2.js
@@ -0,0 +1,50 @@
+import { readFile } from "node:fs/promises";
+
+let index = 50;
+let zeroCount = 0;
+
+async function main() {
+    const data = await readFile("input.txt", "utf-8");
+    const rotations = data.split("\n");
+
+    for (let i = 0; i < rotations.length; i++) {
+        if (rotations[i].startsWith("R")) {
+            let rotationCount = parseInt(rotations[i].slice(1));
+
+            for (let j = 0; j < rotationCount; j++) {
+                if (index == 99) {
+                    index = 0;
+                } else {
+                    index++;
+                }
+                if (index == 0) {
+                    zeroCount++;
+                }
+            }
+        } else if (rotations[i].startsWith("L")) {
+            let rotationCount = parseInt(rotations[i].slice(1));
+
+            for (let j = 0; j < rotationCount; j++) {
+                if (index == 0) {
+                    index = 99;
+                } else {
+                    index--;
+                }
+                if (index == 0) {
+                    zeroCount++;
+                }
+            }
+        } else {
+            console.log(`Unknown rotation: ${rotations[i]}`);
+        }
+
+        console.log(`${rotations[i]}: ended up at ${index}`);
+
+        if (index < 0 || index > 99) {
+            console.log(`Invalid index: ${index}`);
+        }
+    }
+    console.log(`Zero count: ${zeroCount}`);
+}
+
+main();
